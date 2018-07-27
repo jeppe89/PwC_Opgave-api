0d84dca993e65729847d341868cf7818db58ab2a
Jeppe <s093905@student.dtu.dk>
2018-07-27 19:24:16 +0200

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (env('APP_ENV') === 'prod') {
-            \URL::forceScheme('https');
-        }
     }
 
     /**
