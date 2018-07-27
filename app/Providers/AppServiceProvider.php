59845207e9bc7de9f7a3365c3312805aa9376a32
Jeppe <s093905@student.dtu.dk>
2018-07-27 14:13:01 +0200

    Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        $this->app['request']->server->set('HTTPS', true);
+        \URL::forceScheme('https');
     }
 
     /**
