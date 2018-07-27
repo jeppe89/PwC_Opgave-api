9fba4d3c0adc8b3905eb7d4840b8f74be1f20466
Jeppe <s093905@student.dtu.dk>
2018-07-27 17:07:30 +0200

    Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,8 @@ class Cors
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type')
-            ->header('Access-Control-Allow-Credentials', true);
+            ->header('Access-Control-Allow-Credentials', true)
+            ->header('Access-Control-Allow-Request-Headers', 'Authorization');
 
     }
 }
