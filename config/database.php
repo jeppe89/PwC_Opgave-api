2b5610450451dbe13c908cabff1a553bfd7c0a43
jeppe89 <s093905@student.dtu.dk>
2018-07-27 12:40:00 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -49,11 +49,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $host),
-            'port' => '3306'),
-            'database' => $database),
-            'username' => $username),
-            'password' => $password),
+            'host' => $host,
+            'port' => '3306',
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
