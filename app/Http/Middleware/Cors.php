863cf59941e476c5adb3854d6fd5a751979cea90
Jeppe <s093905@student.dtu.dk>
2018-07-27 17:03:52 +0200

    Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,8 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+            ->header('Access-Control-Allow-Headers', 'Content-Type')
+            ->header('Access-Control-Allow-Credentials', true);
+
     }
 }
